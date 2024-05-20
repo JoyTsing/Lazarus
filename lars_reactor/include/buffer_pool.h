f23f0b6d0f4d0b48269475a54c63ac0c1514242a
JoyTsing <792566033@qq.com>
2024-05-20 23:12:59 +0800

    [add] buffer_pool init

--- lars_reactor/include/buffer_pool.h
+++ lars_reactor/include/buffer_pool.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <mutex>
 #include <unordered_map>
 
 #include "io_buf.h"
@@ -8,14 +9,18 @@
 using bpool_t = std::unordered_map<int, IOBuffer*>;
 
 enum class MEM_CAP : std::uint32_t {
-  m4K = 4096,
-  m16K = 16384,
-  m64K = 65536,
-  m256K = 262144,
-  m1M = 1048576,
-  m4M = 4194304,
-  m8M = 8388608
+  m4K = 4096,      // 4K
+  m16K = 16384,    // 16K
+  m64K = 65536,    // 64K
+  m256K = 262144,  // 256K
+  m1M = 1048576,   // 1M
+  m4M = 4194304,   // 4M
+  m8M = 8388608    // 8M 23bit
 };
+
+inline int mem_cap_to_int(MEM_CAP cap) { return static_cast<int>(cap); }
+
+const constexpr std::uint64_t MEM_LIMIT = 5368709120;  // 5G，单位为B
 
 class BufferPool {
  public:
@@ -27,10 +32,19 @@ class BufferPool {
     return _instance;
   }
 
+  // 从内存池申请一个buffer
+  IOBuffer* alloc_buffer();
+  IOBuffer* alloc_buffer(int size);
+
+  // 将buffer放回内存池
+  void revert(IOBuffer* buf);
+
  private:
   BufferPool();
+  void init_buffer_unit(int size, MEM_CAP page_size);
 
  private:
   bpool_t _pool;
   std::uint64_t _total_mem;
+  std::mutex _mutex;
 };
