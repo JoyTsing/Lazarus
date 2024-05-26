8c74b3dbc0cb88dea5c7e7b704b267f1aa01aaa5
JoyTsing <792566033@qq.com>
2024-05-26 18:58:19 +0800

    [add] message router

--- lars_reactor/include/tcp_client.h
+++ lars_reactor/include/tcp_client.h
@@ -4,12 +4,12 @@
 #include <unistd.h>
 
 #include "event_loop.h"
+#include "message.h"
 #include "reactor_buf.h"
 
 class TCPClient;
-
-using message_call_back =
-    std::function<void(const char* data, uint32_t len, int message_id,
+using message_callback =
+    std::function<void(const char* data, std::uint32_t len, int message_id,
                        TCPClient* client, void* user_data)>;
 
 class TCPClient {
@@ -22,7 +22,7 @@ class TCPClient {
   void clear();
 
   // set event callback
-  void set_callback(message_call_back cb) { _message_cb = cb; }
+  void set_callback(message_callback cb) { _message_cb = cb; }
 
  private:
   void handle_connect();
@@ -40,7 +40,7 @@ class TCPClient {
   socklen_t _addr_len;
   EventLoop* _loop;
   // handler
-  message_call_back _message_cb;
+  message_callback _message_cb;
   // buffer
   InputBuffer _input_buf;
   OutputBuffer _output_buf;
