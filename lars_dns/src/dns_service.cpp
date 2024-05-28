820eba4a8d2f7336b7e7855db880fc50e830807a
JoyTsing <792566033@qq.com>
2024-05-28 19:11:36 +0800

    [add] lars_dns init

---
+++ lars_dns/src/dns_service.cpp
@@ -0,0 +1,21 @@
+#include <cstring>
+
+#include "eventloop/event_loop.h"
+#include "net/tcp/tcp_server.h"
+#include "utils/config_file.h"
+
+TcpServer *server;
+
+int main(int argc, const char **argv) {
+  EventLoop loop;
+  config_file::setPath("./conf/lars_dns.conf");
+
+  std::string ip =
+      config_file::instance()->GetString("reactor", "ip", "0.0.0.0");
+  short port = config_file::instance()->GetNumber("reactor", "port", 7777);
+
+  server = new TcpServer(&loop, ip.c_str(), port);
+
+  loop.event_process();
+  return 0;
+}
