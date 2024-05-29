86578d4e5dbcea1ede7726a27a8e631f204efd4f
JoyTsing <792566033@qq.com>
2024-05-29 20:11:56 +0800

    [fix] remove unnecessary library.

--- lars_dns/tests/example_test.cpp
+++ lars_dns/tests/example_test.cpp
@@ -7,16 +7,15 @@
 #include "utils/minilog.h"
 
 // NOLINTNEXTLINE
-TEST_CASE("thread-pool test1") {
+TEST_CASE("test1") {
   minilog::set_log_level(minilog::log_level::warn);
   int iter = 0;
   // 100 times faster than mine, LOL
-  ankerl::nanobench::Bench().minEpochIterations(10).run(
-      "[test1] boost::asio::thread_pool speed test", [&]() {
-        int res = 0;
-        for (int i = 0; i < 10000; i++) {
-          res += i;
-        }
-        minilog::log_warn("epoch {} done, add sum is {}", iter++, res);
-      });
+  ankerl::nanobench::Bench().minEpochIterations(10).run("[test1] test", [&]() {
+    int res = 0;
+    for (int i = 0; i < 10000; i++) {
+      res += i;
+    }
+    minilog::log_warn("epoch {} done, add sum is {}", iter++, res);
+  });
 }
