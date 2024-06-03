1258e5d32a473b54b3b7ea89869a457edfc20a1f
JoyTsing <792566033@qq.com>
2024-06-03 17:09:55 +0800

    [change] try to fix can not build done once.

--- api/build.sh
+++ api/build.sh
@@ -2,4 +2,7 @@
 if [ -d ./build ]; then
   rm build -r
 fi
-cmake -B build && cmake --build build -j 12
+cmake -B build -GNinja && cmake --build build -j 12
+# sometimes not build proto first, so we just need to build again
+# it just works
+cmake --build build -j 12
--- lars_reactor/build.sh
+++ lars_reactor/build.sh
@@ -2,4 +2,4 @@
 if [ -d ./build ]; then
   rm build -r
 fi
-cmake -B build && cmake --build build -j 12
+cmake -B build -GNinja && cmake --build build -j 12
