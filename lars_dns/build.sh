34e0c6f17c5094c98554028ca8e8990b0f113ac3
JoyTsing <792566033@qq.com>
2024-06-03 17:15:48 +0800

    [change] do not build examples when build all.

--- lars_dns/build.sh
+++ lars_dns/build.sh
@@ -3,6 +3,3 @@ if [ -d ./build ]; then
   rm build -r
 fi
 cmake -B build -GNinja && cmake --build build -j 12
-# sometimes not build proto first, so we just need to build again
-# it just works
-cmake --build build -j 12
